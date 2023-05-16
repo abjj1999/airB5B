4be446028b469bfb64e4322df29d018f95e15a92
Abdullah al hilfi <abdullahalhilfi21@gmail.com>
2023-05-16 00:00:42 -0500

    register route is complete

--- app/components/models/RegisterModal.tsx
+++ app/components/models/RegisterModal.tsx
@@ -38,7 +38,7 @@ const RegisterModal = () => {
     const onSubmit: SubmitHandler<FieldValues> =  (data) => {
         console.log("data")
         setIsLoading(true);
-        axios.post("/api/auth/register", data)
+        axios.post("/api/register", data)
             .then(() => {
                 RegisterModal.onClose();
             })
