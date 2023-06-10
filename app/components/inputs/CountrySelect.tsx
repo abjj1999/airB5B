8941e3dcf1304b5bee281b0556198fa0c2c1efa9
Abdullah al hilfi <abdullahalhilfi21@gmail.com>
2023-06-10 01:15:10 -0500

    final2

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -16,51 +16,50 @@ interface CountrySelectProps {
   value?: CountrySelectValue;
   onChange: (value: CountrySelectValue) => void;
 }
-const CountrySelect: React.FC <CountrySelectProps> = ({
-    value,
-    onChange,
+
+const CountrySelect: React.FC<CountrySelectProps> = ({
+  value,
+  onChange
 }) => {
+  const { getAllCountries } = useCountries();
 
-    const {getAllCountries} = useCountries();
-    // console.log(getAllCountries())
-
-    return ( 
-        <div>
-          <Select
-            placeholder="Anywhere"
-            isClearable
-            options={getAllCountries()}
-            value={value}
-            onChange={(value) => onChange(value as CountrySelectValue)}
-            formatOptionLabel={(option: any) => (
-              <div className="
-              flex flex-row items-center gap-3">
-                <div>{option.flag}</div>
-                <div>
-                  {option.label}
-                  <span className="text-neutral-500 ml-1">
-                    {option.region}
-                  </span>
-                </div>
-              </div>
-            )}
-            classNames={{
-              control: () => 'p-3 border-2',
-              input: () => 'text-lg',
-              option: () => 'text-lg'
-            }}
-            theme={(theme) => ({
-              ...theme,
-              borderRadius: 6,
-              colors: {
-                ...theme.colors,
-                primary: 'black',
-                primary25: '#ffe4e6'
-              }
-            })}
-          />
-        </div>
-       );
-    }
+  return ( 
+    <div>
+      <Select
+        placeholder="Anywhere"
+        isClearable
+        options={getAllCountries()}
+        value={value}
+        onChange={(value) => onChange(value as CountrySelectValue)}
+        formatOptionLabel={(option: any) => (
+          <div className="
+          flex flex-row items-center gap-3">
+            <div>{option.flag}</div>
+            <div>
+              {option.label},
+              <span className="text-neutral-500 ml-1">
+                {option.region}
+              </span>
+            </div>
+          </div>
+        )}
+        classNames={{
+          control: () => 'p-3 border-2',
+          input: () => 'text-lg',
+          option: () => 'text-lg'
+        }}
+        theme={(theme) => ({
+          ...theme,
+          borderRadius: 6,
+          colors: {
+            ...theme.colors,
+            primary: 'black',
+            primary25: '#ffe4e6'
+          }
+        })}
+      />
+    </div>
+   );
+}
  
 export default CountrySelect;
